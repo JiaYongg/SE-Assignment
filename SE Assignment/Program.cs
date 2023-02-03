58de52c0cdbd7db720d4e508c9b465bfb336dca9
JiaYongg <s10202579@connect.np.edu.sg>
2023-02-03 15:42:18 +0800

    Added a few guests in main program

--- SE Assignment/Program.cs
+++ SE Assignment/Program.cs
@@ -1,30 +1,55 @@
-﻿while (true)
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SE_Assignment
 {
-
-    int main_choice;
-    // simple login option
-    //Console.WriteLine();
-    Console.WriteLine("------------Log in as------------");
-    Console.WriteLine("1) Guest");
-    Console.WriteLine("2) Hotel Administrator");
-    Console.WriteLine("3) Exit");
-    Console.WriteLine();
-
-    // Validation to ensure the input are NUMBERS ONLY
-    Console.Write("Enter choice: ");
-    string main_input = Console.ReadLine();
-    bool success = Int32.TryParse(main_input, out main_choice);
-    if (!success)
+    class Program
     {
-        Console.WriteLine();
-        Console.WriteLine("Please Enter Integer Input only!");
-        continue;
-    }
+        static void Main(string[] args)
+        {
+            // initialize a few guests
+            Guest guest1 = new Guest("Sam", "1", "93836739", "sam56@outlook.com", "S9584736A", 200.50);
+            Guest guest2 = new Guest("Nat", "2", "83759257", "nat24@gmail.com", "S8347483N", 145.20);
+            Guest guest3 = new Guest("Kim", "3", "84563445", "kim62@hotmail.com", "S9182745E", 50.60);
+            Guest guest4 = new Guest("Lay", "4", "94785965", "lay@connect.np.edu.sg", "T0104957Z", 70.20);
+            Guest guest5 = new Guest("Ken", "5", "93857365", "ken51@gmail.com", "S9858373G", 66.50);
+            Guest guest6 = new Guest("Haz", "6", "81957484", "haz9485@outlook.com", "T0204857J", 10);
 
-    // choosing the options
-    switch (main_choice)
-    {
-        case 1:
-            Console.WriteLine();
+
+            while (true)
+            {
+                int main_choice;
+                // simple login option
+                //Console.WriteLine();
+                Console.WriteLine("------------Log in as------------");
+                Console.WriteLine("1) Guest");
+                Console.WriteLine("2) Hotel Administrator");
+                Console.WriteLine("3) Exit");
+                Console.WriteLine();
+
+                // Validation to ensure the input are NUMBERS ONLY
+                Console.Write("Enter choice: ");
+                string main_input = Console.ReadLine();
+                bool success = Int32.TryParse(main_input, out main_choice);
+                if (!success)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Please Enter Integer Input only!");
+                    continue;
+                }
+
+                // choosing the options
+                switch (main_choice)
+                {
+                    case 1:
+                        Console.WriteLine();
+                        break;
+
+                }
+            }
+        }
     }
 }
