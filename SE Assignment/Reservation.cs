4b957ba83d1b7d384146e5e98f4c7634d2f71c80
Villicus-np <s10207947@connect.np.edu.sg>
2023-02-03 14:58:14 +0800

    Added base attributes & methods for Reservation Class

--- SE Assignment/Reservation.cs
+++ SE Assignment/Reservation.cs
@@ -8,6 +8,80 @@ namespace SE_Assignment
 {
     public class Reservation
     {
+        private string reservationId;
+        private DateTime checkInDate;
+        private DateTime checkOutDate;
+        private int reservationCost;
+        private bool isCancelled;
 
+        public string ReservationId
+        {
+            get { return reservationId; }
+            set { reservationId = value; }
+        }
+
+        public DateTime CheckInDate
+        {
+            get { return checkInDate; }
+            set { checkInDate = value; }
+        }
+
+        public DateTime CheckOutDate
+        {
+            get { return checkOutDate; }
+            set { checkOutDate = value; }
+        }
+
+        //public int reservationCost // Derivated Attribute
+        //{
+        //    get { return reservationCost; }
+        //    set { reservationCost = value; }
+        //}
+
+        public bool IsCancelled
+        {
+            get { return isCancelled; }
+            set { isCancelled = value; }
+        }
+
+        public Reservation() { };
+        public Reservation(string id, DateTime checkIn, DateTime checkout, int costs, bool iscancelled) 
+        {
+            reservationId = id;
+            checkInDate = checkIn; 
+            checkOutDate = checkout;
+            reservationCost = costs;
+            isCancelled = iscancelled;
+        }
+
+        public void checkIn()
+        {
+            CheckInDate = DateTime.Now;
+            Console.WriteLine("Check In Date: "+ CheckInDate);
+        }
+
+        public void checkOut()
+        {
+            CheckOutDate = DateTime.Now;
+            Console.WriteLine("Check Out Date: " + CheckOutDate);
+        }
+
+        public void calculateCost()
+        {
+            //Implementation
+        }
+
+        public bool isRoomAvail()
+        {
+            //if (isRoomAvail)
+            //{
+            //    return true;
+            //}
+            //else
+            //{
+            //    return false;
+            //}
+            return true;
+        }
     }
 }
