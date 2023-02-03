f57326a70a269843c3befbe844fec2d0402872ad
WaiKeat <wklai368@gmail.com>
2023-02-03 15:06:22 +0800

    Create HotelReservation class

---
+++ SE Assignment/HotelReservation.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SE_Assignment
+{
+    class HotelReservation
+    {
+        private string roomNum;
+        private bool roomAvailable;
+        private int maxGuest;
+        private bool hasBrakfast;
+        private string bedType;
+        private int roomCost;
+
+        private Hotel hotel;
+        private Reservation reservation;
+
+        public string roomNum
+        {
+            get { return roomNum; }
+            set { roomNum = value; }
+        }
+        public bool roomAvailable
+        {
+            get { return roomAvailable; }
+            set { roomAvailable = value; }
+        }
+        public int maxGuest
+        {
+            get { return maxGuest; }
+            set { maxGuest = value; }
+        }
+        public bool hasBreakfast
+        {
+            get { return hasBreakfast; }
+            set { hasBreakfast = value; }
+        }
+        public string bedType
+        {
+            get { return bedType; }
+            set { bedType = value; }
+        }
+        public int roomCost
+        {
+            get { return roomCost; }
+            set { roomCost = value; }
+        }
+    }
+}
