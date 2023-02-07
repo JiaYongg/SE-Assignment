9196a16d81006cc53adf07e2130ab4e0e9126d12
JiaYongg <s10202579@connect.np.edu.sg>
2023-02-07 20:19:20 +0800

    Implemented HotelIterator class

---
+++ SE Assignment/HotelIterator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SE_Assignment
+{
+    public class HotelIterator
+    {
+        private List<Hotel> hotels;
+        private int position = 0;
+
+        public HotelIterator(List<Hotel> hotels)
+        {
+            this.hotels = hotels;
+        }
+        public bool hasNext()
+        {
+            if (position < hotels.Count)
+            {
+                return true;
+            }
+            return false;
+        }
+        public object next()
+        {
+            Hotel hotel = hotels[position];
+            position++;
+            while (position < hotels.Count)
+            {
+                position++;
+            }
+            return hotel;
+        }
+        public void remove()
+        {
+            throw new InvalidOperationException("Remove is not supported by Hotel Iterator");
+        }
+    }
+}
